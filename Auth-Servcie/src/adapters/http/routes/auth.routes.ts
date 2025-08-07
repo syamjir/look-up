2404ec6b2a0ada22e2a56dd8d28d0f50963edcad
syamjir <rsyamjir@gmail.com>
2025-08-07 16:32:43 +0530

    docs(router): add comments to express router for better clarity

--- Auth-Servcie/src/adapters/http/routes/auth.routes.ts
+++ Auth-Servcie/src/adapters/http/routes/auth.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import { login, register } from '../controllers/auth.controller'
 
-const router = express.Router()
+const router = express.Router() // express router
 
 router.post('/register', register)
 router.post('/login', login)
