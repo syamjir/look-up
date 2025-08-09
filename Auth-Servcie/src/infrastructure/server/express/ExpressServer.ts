f8d1b300d3fd4ac245aa961a2d96c22a63ea5cef
syamjir <rsyamjir@gmail.com>
2025-08-09 11:22:24 +0530

    feat(security): add rate-limiter, mongo-sanitize, xss sanitizer, compression, and helmet

--- Auth-Servcie/src/infrastructure/server/express/ExpressServer.ts
+++ Auth-Servcie/src/infrastructure/server/express/ExpressServer.ts
@@ -3,6 +3,11 @@ import express, { Application } from 'express'
 import authRoutes from '../../../adapters/http/routes/auth.routes'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import rateLimit from 'express-rate-limit'
+import helmet from 'helmet'
+import mongoSanitize from 'express-mongo-sanitize'
+import { xss } from 'express-xss-sanitizer'
+import compression from 'compression'
 
 dotenv.config()
 
@@ -30,6 +35,18 @@ export class ExpressServer {
     this.app.use(express.json())
     this.app.use(cookieParser())
     this.app.use(cors())
+    //  Security middleware
+    this.app.use(helmet()) // Set security HTTP headers
+    const apiLimiter = rateLimit({
+      // Limit requests from same API
+      max: 100,
+      windowMs: 60 * 60 * 1000,
+      message: 'Too many requests from this IP, please try again in an hour!',
+    })
+    this.app.use('/api', apiLimiter)
+    this.app.use(mongoSanitize()) // Data sanitization against NoSQL query injection
+    this.app.use(xss()) // Data sanitization against XSS
+    this.app.use(compression()) // Enable gzip compression for all responses
   }
 
   private configureRoutes(): void {
