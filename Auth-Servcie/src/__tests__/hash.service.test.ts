2e4c9e6edf25ccfbec5db4172da41c8473a4bbdf
syamjir <rsyamjir@gmail.com>
2025-08-09 09:42:03 +0530

    test(auth): add integration tests for comparePassword without mocking bcrypt

--- Auth-Servcie/src/__tests__/hash.service.test.ts
+++ Auth-Servcie/src/__tests__/hash.service.test.ts
@@ -1,4 +1,4 @@
-import { hashedPassword } from '../shared/utils/hash.service'
+import { hashedPassword, comparePassword } from '../shared/utils/hash.service'
 
 describe('Password Hashing Service', () => {
   const password = 'mySecret123'
@@ -7,5 +7,17 @@ describe('Password Hashing Service', () => {
     const hash = await hashedPassword(password)
     expect(typeof hash).toBe('string')
     expect(hash).not.toBe(password)
+  })
+  const plainPassword = 'mySecret123'
+  it('should return true for correct password', async () => {
+    const hash = await hashedPassword(plainPassword)
+    const result = await comparePassword(plainPassword, hash)
+    expect(result).toBe(true)
+  })
+
+  it('should return false for incorrect password', async () => {
+    const hash = await hashedPassword(plainPassword)
+    const result = await comparePassword('wrongPassword', hash)
+    expect(result).toBe(false)
   })
 })
