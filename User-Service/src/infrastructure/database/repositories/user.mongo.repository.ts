d98405f169ab2407f3807a9afb3e3027c08efea4
syamjir <rsyamjir@gmail.com>
2025-08-10 16:51:55 +0530

    feat(user-repo): implement MongoDB user repository with lean queries for performance

---
+++ User-Service/src/infrastructure/database/repositories/user.mongo.repository.ts
@@ -0,0 +1,27 @@
+import { UpdateUserPayload, User } from '../../../domain/entities/user.entity'
+import { UserRepository } from '../../../domain/repositories/user.repository'
+import { userModel } from '../models/user.model'
+
+export class MongoUserRepository implements UserRepository {
+  async createUser(user: User): Promise<User> {
+    const createdUser = new userModel(user)
+    return createdUser.save()
+  }
+  async findUserByEmail(email: string): Promise<User | null> {
+    return await userModel.findOne({ email }).lean()
+  }
+  async findUserById(id: string): Promise<User | null> {
+    return await userModel.findById(id).lean()
+  }
+  async updateUser(
+    id: string,
+    payload: UpdateUserPayload,
+  ): Promise<User | null> {
+    return await userModel.findByIdAndUpdate(id, payload, { new: true })
+  }
+
+  async deleteUser(id: string): Promise<boolean> {
+    const deletedUser = await userModel.findByIdAndDelete(id)
+    return deletedUser ? true : false
+  }
+}
