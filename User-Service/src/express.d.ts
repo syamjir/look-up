89f1b43549e745b33be72f17b6c7c5d5ca09585b
syamjir <rsyamjir@gmail.com>
2025-08-10 22:57:22 +0530

    chore(types): extend Express Request interface with user property

---
+++ User-Service/src/express.d.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as express from 'express'
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string
+        email: string
+      }
+    }
+  }
+}
