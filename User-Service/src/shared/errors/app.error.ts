8687e5c5e1d80ed5f0a59136ba3224f414290cb5
syamjir <rsyamjir@gmail.com>
2025-08-10 17:53:07 +0530

    feat(error-handling): add AppError class for consistent application errors

---
+++ User-Service/src/shared/errors/app.error.ts
@@ -0,0 +1,15 @@
+import { IAppError } from './error.entity'
+
+export class AppError extends Error implements IAppError {
+  statusCode: number
+  status: string
+  isOperational: boolean
+
+  constructor(message: string, statusCode: number) {
+    super(message)
+    this.statusCode = statusCode
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'
+    this.isOperational = true
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
