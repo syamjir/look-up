4485a48db3d8db9d1428828bd6c2fe31f2b86ea9
syamjir <rsyamjir@gmail.com>
2025-08-13 21:46:06 +0530

    feat(auth): add POST route for token verification

--- Auth-Servcie/src/adapters/http/routes/auth.routes.ts
+++ Auth-Servcie/src/adapters/http/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { login, register } from '../controllers/auth.controller'
+import { login, register, verifyToken } from '../controllers/auth.controller'
 
 const router = express.Router() // express router
 
 router.post('/register', register)
 router.post('/login', login)
+router.post('/verifyToken', verifyToken)
 
 export default router
